1c040f1622607dc67beda63edeedf29eaa9f4650
mohammadshariful <mohammadsharifulislam64@gmail.com>
2022-03-01 02:55:34 +0600

    show the data ui

--- js/main.js
+++ js/main.js
@@ -1,9 +1,7 @@
 // selects elements
+const mainContainer = document.getElementById("mobile-gallary");
 // common function
-// get element
-const getElement = (elmId) => {
-  return document.getElementById(elmId);
-};
+
 // get input value
 const getInputValue = (inputId) => {
   const input = document.getElementById(inputId);
@@ -17,7 +15,50 @@ const toggle = (elmId, display) => {
 };
 // main functionalities here
 // search button
-const searchBtn = () => {
+const searchBtn = async () => {
+  mainContainer.textContent = "";
+  toggle("loading-spinner", "block");
+  toggle("error-msg", "none");
   const inputText = getInputValue("search-input");
-  console.log(inputText);
+  // fetch api
+  const url = `https://openapi.programming-hero.com/api/phones?search=${inputText}`;
+
+  const res = await fetch(url);
+  const data = await res.json();
+  // conditional checking
+  if (data.status === false || inputText === "") {
+    toggle("loading-spinner", "none");
+    toggle("error-msg", "block");
+  } else {
+    displayData(data.data);
+  }
+};
+// display Data
+const displayData = (phones) => {
+  const mainContainer = document.getElementById("mobile-gallary");
+  mainContainer.textContent = "";
+  const slicePhones = phones.slice(0, 20);
+  slicePhones.forEach((phone) => {
+    const { image, phone_name, brand, slug } = phone;
+    const div = document.createElement("div");
+    div.classList.add("col");
+    div.innerHTML = `
+        <div class="card h-100 py-3">
+                <img src="${image}" class="d-block mx-auto pt-5 " alt="..." />
+                <div class="card-body">
+                  <h5 class="card-title">Name : ${phone_name}</h5>
+                  <p class="card-text">Brand : ${brand} </p>
+                  <button class="btn btn-success" data-bs-toggle="modal"
+                  data-bs-target="#exampleModal" onclick="detailsBtn('${slug}')">More Details</button>
+                </div>
+              </div>
+        `;
+    mainContainer.appendChild(div);
+  });
+  toggle("loading-spinner", "none");
+};
+
+// details btn
+const detailsBtn = (phoneId) => {
+  console.log(phoneId);
 };
